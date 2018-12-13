387c1cbcb4861352cf7af21cd45b7ec1f8fd4aa2
szabadee <kreativ.szabadi@gmail.com>
2018-12-13 09:19:55 +0100

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppModel } from './app.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task-manager';
+  
+  public appModel = new AppModel();
+
+  constructor() {
+  }
+
+  public showAppModelValue() {
+    console.log(this.appModel);
+  }
+
+  public changeChecked(isChecked: boolean) {
+    this.appModel.checked = !!isChecked;
+  }
+
 }
+
---
+++ src/app/app.model.ts
@@ -0,0 +1,11 @@
+export class AppModel {
+
+  constructor (
+    public title?: string,
+    public checked: boolean = false,
+    public category?: string,
+    public text?: string 
+  ) {
+
+  }
+}
